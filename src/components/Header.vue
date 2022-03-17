d1fb0fd8c390e530d2b855096569915b70a11a34
Simon Damberg <damberg.simon00@gmail.com>
2022-03-17 23:24:02 +0100

    Tasks visible

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button text="Add Task" color="green"/>
+    </header>
+</template>
+
+<script>
+import Button from './Button.vue' 
+
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'header-item',
+    props: {
+        title: String
+    },
+    components: {
+        Button
+    }
+}
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 30px;
+}
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-for="task in tasks" :key="task.id">
+      <Task :task="task"/>
+  </div>   
+</template>
+
+<script>
+import Task from './Task.vue'
+
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Tasks',
+    props: {
+        tasks: Array
+    },
+    components: {
+        Task
+    }
+}
+</script>
+
+<style>
+
+</style>
